13042d7547dcb647b4b042921b935deda0579fdd
roselandroche <r.landroche@gmail.com>
2019-11-07 20:30:58 -0500

    Delete functionality working

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -33,6 +33,27 @@ export default class Movie extends React.Component {
     addToSavedList(this.state.movie);
   };
 
+  handleDelete = (event, id) => {
+    event.preventDefault()
+
+    window.confirm('Do you want to delete this forever?')
+    console.log(this.state)
+    const deleted = this.state.movie
+
+    // this.setState(this.state.movie.filter(film => film.id !== id))
+
+    axios
+      .delete(`http://localhost:5000/api/movies/${id}`)
+      .then(res => {
+        console.log(`Movie has been deleted.`)
+        this.props.history.push('/')
+      })
+      .catch(err => {
+        console.log(err)
+        this.setState([ ...this.state.movie, deleted])
+      })
+  }
+
   render() {
     if (!this.state.movie) {
       return <div>Loading movie information...</div>;
@@ -47,6 +68,7 @@ export default class Movie extends React.Component {
         <div key={this.state.movie.id}>
           <Link to={`/update-movie/${this.state.movie.id}`} className='edit-button'>Edit</Link>
         </div>
+        <div className='delete-button' onClick={(event) => this.handleDelete(event, this.state.movie.id)}>Delete</div>
       </div>
     );
   }
